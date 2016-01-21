f64c9ac6ce8d040c5f335eba43a460894346e4e8
alessandro chinazzo <alessandro.chinazzo@gmail.com>
2016-01-21 09:55:28 +0100

    Italian lexicon

---
+++ core/components/geotv/lexicon/it/default.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * it default topic lexicon file for GeoTV extra
+ *
+ * @package geotv
+ */
+
+$_lang['geotv'] = 'GeoTV';
+$_lang['geotv_geofence'] = 'Area geografica';
+$_lang['geotv_geopoint'] = 'Punto geografico';
+
+$_lang['geotv.loading'] = 'Attendere. Sto caricando la mappa...';
+$_lang['geotv.clear'] = 'Elimina tutte le location';
+
+$_lang['geotv.allowMultiple'] = 'Valori multipli';
+$_lang['geotv.allowMultipleDesc'] = 'Permetti all\'utente di specificare valori multipli per questo campo';
+$_lang['geotv.centerLat'] = 'Latitudine di default';
+$_lang['geotv.centerLatDesc'] = 'La latitudine di default del centro della mappa (in gradi decimali)';
+$_lang['geotv.centerLng'] = 'Longitudine di default';
+$_lang['geotv.centerLngDesc'] = 'La longitudine di default del centro della mappa (in gradi decimali)';
+$_lang['geotv.zoom'] = 'Zoom di default';
+$_lang['geotv.zoomDesc'] = 'Il livello di default dello zoom';
+
+$_lang['geotv.wrapperTpl'] = 'Template wrapper';
+$_lang['geotv.wrapperTplDesc'] = 'Nome del chunk da usare come output wrapper';
+$_lang['geotv.areaTpl'] = 'Template per l\'area';
+$_lang['geotv.areaTplDesc'] = 'Nome del chunk da usare come output template';
+$_lang['geotv.areaSep'] = 'Separatore per l\'area';
+$_lang['geotv.areaSepDesc'] = 'Stringa che separa le singole aree';
+$_lang['geotv.pointTpl'] = 'Template per il punto';
+$_lang['geotv.pointTplDesc'] = 'Nome del chunk da usare come output template del punto';
+$_lang['geotv.pointSep'] = 'Separatore per il punto';
+$_lang['geotv.pointSepDesc'] = 'Stringa che separa i singoli punti';
