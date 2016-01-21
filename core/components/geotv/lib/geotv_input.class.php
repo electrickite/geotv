a6cffee05a7076bed70127ad2ccc3e49c2ee0cfe
Corey Hinshaw <hinshaw.25@osu.edu>
2016-01-20 22:21:45 -0500

    Fixes #1 by encoding numeric parameters for Smarty templates

--- core/components/geotv/lib/geotv_input.class.php
+++ core/components/geotv/lib/geotv_input.class.php
@@ -5,9 +5,9 @@ if(!class_exists('GeoTVInputRender')) {
             $multiple = $this->modx->getOption('allowMultiple', $params, 1, true);
 
             $params['allowMultiple'] = ($multiple === 0 || $multiple === 'false') ? 0 : 1;
-            $params['centerLat'] = floatval($this->modx->getOption('centerLat', $params, 39, true));
-            $params['centerLng'] = floatval($this->modx->getOption('centerLng', $params, -95, true));
-            $params['zoom'] = intval($this->modx->getOption('zoom', $params, 3, true));
+            $params['centerLat'] = json_encode(floatval($this->modx->getOption('centerLat', $params, 39, true)));
+            $params['centerLng'] = json_encode(floatval($this->modx->getOption('centerLng', $params, -95, true)));
+            $params['zoom'] = json_encode(intval($this->modx->getOption('zoom', $params, 3, true)));
 
             $lang = $this->modx->lexicon->fetch('geotv.', true);
             $this->modx->smarty->assign('geotv', $lang);
