e5b7d8fb437c5edde0ba4b88ac5b56c22bb54ad7
Vierkante Meter <mark@vierkante-meter.nl>
2016-01-25 12:38:43 +0100

    Create default.inc.php

---
+++ core/components/geotv/lexicon/nl/default.inc.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * nl default topic lexicon file for GeoTV extra
+ *
+ * @package geotv
+ */
+
+$_lang['geotv'] = 'GeoTV';
+$_lang['geotv_geofence'] = 'Geografisch gebied';
+$_lang['geotv_geopoint'] = 'Geografisch punt';
+
+$_lang['geotv.loading'] = 'Een ogenblik geduld, de kaart wordt geladen...';
+$_lang['geotv.clear'] = 'Verwijder alle markeringen';
+
+$_lang['geotv.allowMultiple'] = 'Meerdere markeringen toestaan';
+$_lang['geotv.allowMultipleDesc'] = 'Sta toe om meerdere markeringen op te geven';
+$_lang['geotv.centerLat'] = 'Standaard breedtegraad';
+$_lang['geotv.centerLatDesc'] = 'De breedtegraad waar de kaart standaard op wordt gecentreerd (in decimale graden, bijv.: 45.0040)';
+$_lang['geotv.centerLng'] = 'Standaard lengtegraad';
+$_lang['geotv.centerLngDesc'] = 'De lengtegraad waar de kaart standaard op wordt gecentreerd (in decimale graden, bijv.: 26.0027)';
+$_lang['geotv.zoom'] = 'Standaard zoomniveau';
+$_lang['geotv.zoomDesc'] = 'Het niveau waar de kaart standaard op wordt ingezoomd';
+
+$_lang['geotv.wrapperTpl'] = 'Wrapper template';
+$_lang['geotv.wrapperTplDesc'] = 'Naam van de chunk die gebruikt wordt voor de output wrapper';
+$_lang['geotv.areaTpl'] = 'Template geografisch gebied';
+$_lang['geotv.areaTplDesc'] = 'Naam van de chunk die gebruikt wordt voor de template van een geografisch gebied';
+$_lang['geotv.areaSep'] = 'Scheidingsteken geografisch gebied';
+$_lang['geotv.areaSepDesc'] = 'Het scheidingsteken dat gebruikt wordt om afzonderlijke gebieden te scheiden';
+$_lang['geotv.pointTpl'] = 'Template geografisch punt';
+$_lang['geotv.pointTplDesc'] = 'Naam van de chunk die gebruikt wordt voor de template van een geografisch punt';
+$_lang['geotv.pointSep'] = 'Scheidingsteken geografisch punt';
+$_lang['geotv.pointSepDesc'] = 'Het scheidingsteken dat gebruikt wordt om afzonderlijke punten te scheiden';
+$_lang['geotv.decimalSep'] = 'Decimaalscheidingsteken';
+$_lang['geotv.decimalSepDesc'] = 'Het scheidingsteken dat gebruikt wordt in decimalen';
