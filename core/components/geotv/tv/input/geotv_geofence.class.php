674dd725f09cbc66efb5af1f8642dfc6e68aaa8a
Richard Hitier <richard.hitier@co-libri.org>
2018-04-10 15:52:59 +0200

    Use osm template in fence renderer if global param set

--- core/components/geotv/tv/input/geotv_geofence.class.php
+++ core/components/geotv/tv/input/geotv_geofence.class.php
@@ -4,7 +4,11 @@ require_once dirname(__FILE__) . '/../../lib/geotv_input.class.php';
 if(!class_exists('GeoTVFenceInputRender')) {
     class GeoTVFenceInputRender extends GeoTVInputRender {
         public function getTemplate() {
-            return $this->modx->getObject('modNamespace','geotv')->getCorePath().'tv/input/tpl/geotv_geofence.tpl';
+            $osm = $this->modx->getOption('geotv.allow_osm');
+            if( $osm == 1 )
+                return $this->modx->getObject('modNamespace','geotv')->getCorePath().'tv/input/tpl/geotv_geofence_osm.tpl';
+            else
+                return $this->modx->getObject('modNamespace','geotv')->getCorePath().'tv/input/tpl/geotv_geofence.tpl';
         }
     }
 }
